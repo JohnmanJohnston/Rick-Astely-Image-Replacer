4d078c9363e1f1b12fbccf831f2c3c90db1fc839
Ethan John <97091148+JohnmanJohnston@users.noreply.github.com>
2022-06-04 17:31:20 +0400

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,25 @@
+// We get the picture of Rick Astely with the url: https://variety.com/wp-content/uploads/2021/07/Rick-Astley-Never-Gonna-Give-You-Up.png?w=1024
+
+var PerformReplacementImages = document.getElementById("change-color");
+
+PerformReplacementImages.addEventListener("click", async () => {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: SetImagesToRick,
+    });
+});
+
+function SetImagesToRick() {
+    var Images = document.getElementsByTagName("img");
+    document.body.style.backgroundColor = "blue";
+    document.body.style.color = "red";
+
+    // document.body.style.fontFamily = "cursive";
+    document.body.style.setProperty("font-family", "'Comic Sans MS', 'Comic Sans', cursive", "important");
+
+    for (var i = 0; i < Images.length; i++) {
+        Images[i].src = "https://variety.com/wp-content/uploads/2021/07/Rick-Astley-Never-Gonna-Give-You-Up.png?w=1024";
+    }
+}
